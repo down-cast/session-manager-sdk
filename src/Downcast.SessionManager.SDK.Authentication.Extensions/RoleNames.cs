eca26c894ed9bca5cc80e9598aff41b0e16dda20
Mihail Brinza <mihail.brinza96@hotmail.com>
2022-07-04 12:16:04 +0100

    add RoleNames.cs

---
+++ src/Downcast.SessionManager.SDK.Authentication.Extensions/RoleNames.cs
@@ -0,0 +1,8 @@
+namespace Downcast.SessionManager.SDK.Authentication.Extensions;
+
+public class RoleNames
+{
+    public const string Admin = "admin";
+    public const string Member = "member";
+    public const string Author = "author";
+}
